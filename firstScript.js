50b27e35bee3cabd9b5f012e8fac617beb71d33d
Iliyas Robin <iliyasahmedr@gmail.com>
2024-12-31 21:29:18 +0600

    updates

--- firstScript.js
+++ firstScript.js
@@ -1,2 +1,22 @@
 //printing first code of JavaScript
-console.log("hello JavaScript");
+console.log("Hello JavaScript");
+
+// let, var and const
+//let : can't be re-declared but can be updated.
+//var : can be re-declared and can be updated.
+//const : can't be re-declared and can't be updated.
+
+let age = 23;
+// let age = 25; //error re declaring
+age = 26; // no error updating
+console.log(age);
+var name = "Iliyas";
+var name = "Robin" //no error re declaring
+//but this is a problem
+name = "Ahmed"; //no error updating
+console.log(name);
+
+const dad = "Younus ali";
+// const dad = "Younus ali"; //error re declaring
+// dad = "Md"; //error updating
+
