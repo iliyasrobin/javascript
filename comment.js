6bcadd81258655ab739e7bbe46ea3ecca85b49db
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-01 12:36:44 +0600

    Add files via upload

---
+++ comment.js
@@ -0,0 +1,6 @@
+//Comments
+
+//This is single line comment
+
+/*This is multi line
+comment as You can see this. */
---
+++ operators.js
@@ -0,0 +1,25 @@
+//Arithmetic Operations
+let a = 43;
+let b = 54;
+let c = a+b; //add
+let d = a-b; //substract
+let e = a*b; //multiply
+let f = a/b; //divide
+console.log("a + b = ",c);
+console.log("a - b = ",d);
+console.log("a * b = ",e);
+console.log("a / b = ", f);
+
+// Modulus
+console.log("a % b = ", a % b);
+
+//Exponentiation
+console.log("a**b = ",a**b);
+
+//Unary Operators
+
+//Increment
+console.log("Increment: ", c++);
+
+//decrement
+console.log("decrement: ", c--);
