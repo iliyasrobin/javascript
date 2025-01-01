77ce05d92db16a98594a154cd7ca568ffed9f394
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-01 17:06:23 +0600

    Update exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -1,5 +1,6 @@
 //alert("hello world");
 
+//divisible by 5 or not
 //taking user input using prompt
 let num = prompt("Enter a number: ");
 
@@ -8,3 +9,21 @@ if (num % 5 === 0) {
 } else {
   alert("Not divisible by 5");
 }
+
+//grade of student
+
+let marks = prompt("Enter your marks: ");
+let grade;
+if (marks >= 80 && marks <= 100) {
+  grade = "A";
+} else if (marks >= 70 && marks <= 79) {
+  grade = "B";
+} else if (marks >= 60 && marks <= 69) {
+  grade = "C";
+} else if (marks >= 50 && marks <= 59) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+
+console.log("Your grade is: " + grade);
