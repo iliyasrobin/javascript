e744486fa950d96262c062c4f5aed7423e3b9a35
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-03 15:43:32 +0600

    updates

---
+++ exercise4.js
@@ -0,0 +1,7 @@
+// Generate usename start with @ fullname and length
+//no spaces
+// iliyasahmed  => @iliyasahmed11
+// "@"+fullname+length
+
+let fullname = prompt("Enter your full name: ");
+let username = "@" + fullname + fullname.length;
--- stringMethods.js
+++ stringMethods.js
@@ -8,11 +8,11 @@
 //str.charAt() - returns the character at a specified index (position) in a string
 //str.charCodeAt() - returns the Unicode of the character at a specified index in a string
 //str.split() - splits a string into an array of substrings
-//str.slice() - extracts a part of a string and returns a new string
+//str.slice(start, end) - extracts a part of a string and returns a new string
 //str.substring() - extracts the characters from a string, between two specified indices
 //str.substr() - extracts the number of characters from a string, starting at a specified index
 //str.replace() - replaces a specified value with another value in a string
-//str.concat() - joins two or more strings
+//str.concat(str2) - joins two or more strings
 //str.search() - searches a string for a specified value and returns the position of the match
 //str.match() - searches a string for a match against a regular expression, and returns the matches
 //str.includes() - checks if a string contains the specified string/characters
@@ -35,5 +35,43 @@ console.log(result2); // hello world!
 // Example 3
 
 let str3 = "   Hello World!   ";
-let result3 = str.trim();
+let result3 = str.trim(); // removes whitespace from both ends of a string
 console.log(result3); // Hello World!
+
+// Example 4
+
+let str4 = "Hello World!";
+let result4 = str.slice(3, 10); //last index not included
+console.log(result4); // lo Worl
+
+// Example 5
+let str5 = "Hello World! ";
+let str6 = "JS";
+let result5 = str5.concat(str6);
+console.log(result5); // Hello World! JS
+console.log(str5 + str6); // Hello World! JS
+
+let str7 = "Hello World! " + 123;
+console.log(str7); // Hello World! 123
+
+// Example 6
+
+let str8 = "Hello World!";
+let result6 = str8.replace("World", "JavaScript");
+console.log(result6); // Hello JavaScript!
+let result7 = str8.replaceAll("World", "JavaScript");
+console.log(result7); // Hello JavaScript!
+
+// Example 7
+
+let str9 = "Hello World!";
+let result8 = str9.charAt(6);
+console.log(result8); // W
+
+// Example 8
+
+let str10 = "Hello World!";
+let a = (str10[4] = "a");
+let b = str10.replace("o", "a");
+console.log(a); // Hello World!
+console.log(b); // Hella World!
