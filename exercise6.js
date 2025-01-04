831892362d3fc3fc4d26df8dade772235916e152
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-04 22:52:20 +0600

    updates

--- exercise6.js
+++ exercise6.js
@@ -14,3 +14,16 @@ companies.splice(2, 1, "Ola"); //remove the 3rd element and add "Ola"
 companies.push("Amazon"); //add "Amazon" to the end of the array
 
 console.log(companies);
+
+//example 2
+const companies2 = [
+  "Bloomberg",
+  "Microsoft",
+  "Uber",
+  "Google",
+  "IBM",
+  "Netflix",
+];
+
+companies2.unshift(); //remove the first element
+console.log(companies2);
