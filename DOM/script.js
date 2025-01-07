a155716736d50337774b007157c23bf5ac74cc1a
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-07 21:10:45 +0600

    updating

--- DOM/script.js
+++ DOM/script.js
@@ -3,3 +3,12 @@
 let h2 = document.querySelector("h2");
 h2.innerText = h2.innerText + " from learning "; //concat
 
+let divs = document.querySelectorAll(".box");
+let idx = 1;
+for (let div of divs) {
+  div.innerText = `new unique value ${idx}`;
+  idx++;
+}
+// divs[0].innerText = "new unique value 1";
+// divs[1].innerText = "new unique value 2";
+// divs[2].innerText = "new unique value 3";
