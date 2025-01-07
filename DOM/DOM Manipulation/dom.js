c1699b6152fa3ebcb26a51a9fe257bacba807e3c
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-07 20:27:36 +0600

    updating

---
+++ DOM/DOM Manipulation/dom.js
@@ -0,0 +1,10 @@
+//Dom Manipulation
+
+//selecting with id
+//document.getElementById("myId");
+
+//selecting with class name
+//document.getElementsByClassName("myClass");
+
+//selecting with tag name
+//document.getElementsByTagName("p");
