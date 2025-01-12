93704be5e686deaaeaeb1e02f5e5b6d172b8b804
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-12 20:53:21 +0600

    updating

--- OOP/class.js
+++ OOP/class.js
@@ -1,1 +1,23 @@
 //Class
+class ToyotaCar {
+  constructor() {
+    console.log("creating new obj");
+    this.brand = brand;
+  }
+  start() {
+    console.log("Start");
+  }
+  stop() {
+    console.log("stop");
+  }
+
+  //   setBrand(brand) {
+  //     this.brand = brand;
+  //   }
+}
+
+let fortuner = new ToyotaCar("fortuner"); //constructor parameter passing
+//fortuner.setBrand("fortuner");
+
+let lexus = new ToyotaCar("lexus"); //constructor parameter passing
+//lexus.setBrand("lexus");
