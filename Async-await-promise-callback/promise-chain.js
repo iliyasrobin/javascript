663cb51ad8489083c02eb4dfe1a6e45c7e3fe13b
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-16 19:53:20 +0600

    updating

--- Async-await-promise-callback/promise-chain.js
+++ Async-await-promise-callback/promise-chain.js
@@ -1,1 +1,45 @@
 //Promise Chain
+
+function asyncFunc1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data 1");
+      resolve("success");
+    }, 4000);
+  });
+}
+
+function asyncFunc2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data 2");
+      resolve("success");
+    }, 4000);
+  });
+}
+
+// console.log("fetching data 1.....");
+// let p1 = asyncFunc1();
+// p1.then((res) => {
+//   console.log(res);
+// });
+
+// console.log("fetching data 2.....");
+// let p2 = asyncFunc2();
+// p2.then((res) => {
+//   console.log(res);
+// });
+
+//in this case both will fetch at same time
+
+//promise chain
+console.log("fetching data 1.....");
+let p1 = asyncFunc1();
+p1.then((res) => {
+  console.log(res);
+  console.log("fetching data 2.....");
+  let p2 = asyncFunc2();
+  p2.then((res) => {
+    console.log(res);
+  });
+});
