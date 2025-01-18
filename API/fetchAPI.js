c87d3770c303f6cb80d648425a9025e2cd910aab
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-18 19:10:09 +0600

    updating

---
+++ API/fetchAPI.js
@@ -0,0 +1,10 @@
+//Fetch API
+//Application Programming Interface
+
+const url = "https://cat-fact.herokuapp.com/facts";
+
+const getFacts = async () => {
+  console.log("getting data");
+  let promise = await fetch(url);
+  console.log(promise);
+};
