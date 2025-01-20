a95b97895ed009071ce8acb03d945527e01f812f
Iliyas Robin <iliyasahmedr@gmail.com>
2025-01-20 18:59:18 +0600

    updating

--- API/fetchAPI.js
+++ API/fetchAPI.js
@@ -5,25 +5,25 @@ const url = " https://restcountries.com/v3.1/all";
 const countries = document.querySelector("#country");
 const btn = document.querySelector("#btn");
 
-// const getCountries = async () => {
-//   console.log("getting data");
-//   let response = await fetch(url);
-//   console.log(response);
-//   let data = await response.json();
-//   //console.log(data);
-//   countries.innerText = data[200].capital;
-// };
+const getCountries = async () => {
+  console.log("getting data");
+  let response = await fetch(url);
+  console.log(response);
+  let data = await response.json();
+  console.log(data);
+  countries.innerText = data[200].name.common;
+};
 
 //or using promise chaining
 //not recommended
-function getCountries() {
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
-}
+// function getCountries() {
+//   fetch(url)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     });
+// }
 
 btn.addEventListener("click", getCountries);
